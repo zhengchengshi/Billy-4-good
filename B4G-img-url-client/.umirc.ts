7707411b03769d802f729f0f7e94740d500d7158
zhengchengshi <2650744390@qq.com>
2022-11-12 13:11:31 +0800

    feat(img-url-client): 替换ico

--- B4G-img-url-client/.umirc.ts
+++ B4G-img-url-client/.umirc.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   // layout: {},
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  links: [
+    {
+      rel: 'icon',
+      href: 'https://file.bytedance.cool/img/158232067acba7fec35dbe2ba953725c.ico',
+    },
+  ],
 });
